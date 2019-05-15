f26b368e015d73565c08c77849c67545c8c7be0f
Dawid <dawid.debo2.pl@gmail.com>
2019-05-15 19:36:39 +0200

    dodano

---
+++ app.js
@@ -0,0 +1,47 @@
+
+var clicks = 0; 
+function countClicks() {
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+};
+function discountClicks() {
+    clicks -= 1;
+    document.getElementById("clicks").innerHTML = clicks;
+}
+//----------------------------------------------------------------
+var clicks = 0; 
+function countClicks1() {
+    clicks += 1;
+    document.getElementById("clicks1").innerHTML = clicks;
+};
+function discountClicks1() {
+    clicks -= 1;
+    document.getElementById("clicks1").innerHTML = clicks;
+}
+//----------------------------------------------------------------            
+var clicks = 0; 
+function countClicks() {
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+};
+function discountClicks() {
+    clicks -= 1;
+    document.getElementById("clicks").innerHTML = clicks;
+}            
+//----------------------------------------------------------------            
+var clicks = 0; 
+function countClicks() {
+    clicks += 1;
+    document.getElementById("licznik_timer").innerHTML = clicks;
+};
+function discountClicks() {
+    clicks -= 1;
+    document.getElementById("licznik_timer").innerHTML = clicks;
+}
+//----------------------------------------------------------------
+let minut = Match.floor(czas / 60)
+let sekundy = czas % 60
+
+function countMinuty() {
+    
+}
